fb55696b91124981a9969ea3927b272a3f2a57d3
Lucas Pinto <lucas.lersp@gmail.com>
2022-09-06 21:43:19 -0300

    Pequena edicao

--- flutter/my_app_test/lib/main.dart
+++ flutter/my_app_test/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'My App to Test install'),
+      home: const MyHomePage(title: 'Mostrar para o Matheuzinho'),
     );
   }
 }
